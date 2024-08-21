6fd91607d6dde15d883479d11dac45d386dae222
adithravindran0fficial <adithravindranofficial@gmail.com>
2024-08-21 16:28:55 +0530

    Add project files.

---
+++ LINQ Task-1/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Task_1
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int  Age{ get; set; }
+        public string City { get; set; }
+        public Person(string name, int age ,string city) {
+            Name = name;
+            Age = age;
+            City = city;
+        }
+    }
+}
---
+++ LINQ Task-1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LINQ_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> list = new List<Person>
+            {
+                new Person("adith",21,"calicut"),
+                new Person("anandhu",21,"kochi"),
+                new Person("nandhu",20,"kottayam"),
+                new Person("sharan",45,"tvm"),
+                new Person("abdulla",34,"kannur"),
+                new Person("irfan",15,"kzd")
+
+            };
+
+            var oldpeople = from person in list where person.Age >= 25 select person;
+
+            foreach(Person people in oldpeople)
+            {
+                Console.WriteLine(people.Name+" "+people.Age);
+            }
+
+        }
+    }
+}
